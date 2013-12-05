5f0d5892ea14549fc2a3d416c294ad7ae460de2e
Alex Burck <burck1@illinois.edu>
2013-12-05 17:56:23 -0600

    removed tesseract, now handled by server

--- tess-two/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/burck1/android-projects/MTD_Companion/tess-two/bin/AndroidManifest.xml : \
-/home/burck1/android-projects/MTD_Companion/tess-two/AndroidManifest.xml \
--- tess-two/bin/classes/R.java.d
+++
@@ -1,1 +0,0 @@
- : /home/burck1/android-projects/MTD_Companion/tess-two/bin/AndroidManifest.xml \
