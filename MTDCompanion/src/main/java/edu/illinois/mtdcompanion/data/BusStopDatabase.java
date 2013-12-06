707af8397c6d6c6ed120a1a2e7f4316954268f71
Tom Olson <tcolson2@illinois.edu>
2013-12-05 22:07:35 -0600

    1. Finished implementing database for storing bus stops.

--- MTDCompanion/src/main/java/edu/illinois/mtdcompanion/data/BusStopDatabase.java
+++ MTDCompanion/src/main/java/edu/illinois/mtdcompanion/data/BusStopDatabase.java
@@ -5,13 +5,13 @@ import java.sql.SQLException;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
-import edu.illinois.mtdcompanion.models.BusRoute;
 import edu.illinois.mtdcompanion.models.BusStop;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 
@@ -41,6 +41,11 @@ public class BusStopDatabase extends OrmLiteSqliteOpenHelper {
 	 * Version number of database schema used to store bus stops
 	 */
 	private static final int DATABASE_VERSION = 1;
+
+	/**
+	 * Database Access Object (DAO) for BusStop
+	 */
+	private Dao<BusStop, Integer> dao = null;
 
 	/**
 	 * Default constructor for singleton
@@ -73,7 +78,6 @@ public class BusStopDatabase extends OrmLiteSqliteOpenHelper {
 		try {
 			// Create database tables
 			TableUtils.createTable(connectionSource, BusStop.class);
-			TableUtils.createTable(connectionSource, BusRoute.class);
 
 			logger.debug("All tables successfully created onCreate");
 		} catch (SQLException e) {
@@ -92,7 +96,6 @@ public class BusStopDatabase extends OrmLiteSqliteOpenHelper {
 		try {
 			// Drop old version of database
 			TableUtils.dropTable(connectionSource, BusStop.class, true);
-			TableUtils.dropTable(connectionSource, BusRoute.class, true);
 
 			logger.debug("All tables successfully dropped onUpgrade");
 		} catch (SQLException e) {
@@ -102,6 +105,37 @@ public class BusStopDatabase extends OrmLiteSqliteOpenHelper {
 
 		// Recreate database using new version
 		onCreate(database, connectionSource);
+	}
+
+	/**
+	 * Returns Database Access Object (DAO) for BusStop <br>
+	 * DAO is created once and then cached
+	 * @return {@link #dao}
+	 */
+	public Dao<BusStop, Integer> getBusStopDAO() {
+		if (dao == null) {
+			try {
+				// Get DAO that represents BusStop
+				dao = getDao(BusStop.class);
+			} catch (SQLException e) {
+				logger.error("Unable to get DAO for BusStop");
+				e.printStackTrace();
+			}
+		}
+
+		return dao;
+	}
+
+	/**
+	 * Close database connection <br>
+	 * Clears {@link #dao}
+	 */
+	@Override
+	public void close() {
+		super.close();
+
+		dao = null;
+		instance = null;
 	}
 
 }
