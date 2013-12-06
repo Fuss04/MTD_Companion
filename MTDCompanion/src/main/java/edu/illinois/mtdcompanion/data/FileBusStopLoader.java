771a21fd46fae21a8125c757d22ec109ea0bba31
Tom Olson <tcolson2@illinois.edu>
2013-12-06 03:36:31 -0600

    1. Implemented json parsing.

--- MTDCompanion/src/main/java/edu/illinois/mtdcompanion/data/FileBusStopLoader.java
+++ MTDCompanion/src/main/java/edu/illinois/mtdcompanion/data/FileBusStopLoader.java
@@ -1,22 +1,20 @@
 package edu.illinois.mtdcompanion.data;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 
 import android.content.Context;
 import android.content.res.AssetManager;
 
 import edu.illinois.mtdcompanion.models.BusStop;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 /**
  * Loads questions in database from jSON file <br>
@@ -76,7 +74,65 @@ public class FileBusStopLoader {
 			e.printStackTrace();
 		}
 
+		JSONObject jsonObject = null;
+
+		try {
+			BufferedReader bufferedReader = null;
+
+			bufferedReader = new BufferedReader(new InputStreamReader(is));
+			StringBuffer content = new StringBuffer();
+			char[] buffer = new char[1024];
+			int number;
+
+			while ((number = bufferedReader.read(buffer)) > 0) {
+				content.append(buffer, 0 , number);
+			}
+
+			jsonObject = new JSONObject(content.toString());
+		} catch (IOException e) {
+			logger.error("IO Error Creating JSONObject");
+			e.printStackTrace();
+		} catch (JSONException e) {
+			logger.error("JSON Error creating JSONObject");
+			e.printStackTrace();
+		}
+
+		JSONArray jsonArray = null;
+
+		database.open(context);
+
+		try {
+			jsonArray = jsonObject.getJSONArray("stops");
+
+			for (int index = 0; index < jsonArray.length(); index++) {
+				JSONObject object = jsonArray.getJSONObject(index);
+
+				String stopID = object.getString("stop_id");
+				String stopCode = object.getString("code");
+				String stopName = object.getString("stop_name");
+
+				String latitude = object.getJSONArray("stop_points").getJSONObject(0).getString("stop_lat");
+				String longitude = object.getJSONArray("stop_points").getJSONObject(0).getString("stop_lon");
+
+				BusStop newStop = new BusStop();
+				newStop.setStopID(stopID);
+				newStop.setStopCode(stopCode);
+				newStop.setStopName(stopName);
+				newStop.setLatitude(Double.parseDouble(latitude));
+				newStop.setLongitude(Double.parseDouble(longitude));
+
+				database.create(newStop);
+			}
+		} catch (JSONException e) {
+			logger.error("JSON Error parsing JSONObject");
+			e.printStackTrace();
+		}
+
+		database.close();
+
+		/*
 		InputStreamReader reader = new InputStreamReader(is, Charset.defaultCharset());
+		BufferedReader buffer = new BufferedReader(reader);
 
 		JsonParser parser = new JsonParser();
 
@@ -107,6 +163,7 @@ public class FileBusStopLoader {
 		}
 
 		database.close();
+		 */
 	}
 
 }
